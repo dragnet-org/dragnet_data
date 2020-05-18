928b19b9b41615b5608a6adf9d7e3fa761b90a09
Burton DeWilde <burtdewilde@gmail.com>
2020-05-18 11:21:20 -0500

    Add text and extend json save/load utils

--- src/dragnet_data/utils.py
+++ src/dragnet_data/utils.py
@@ -1,3 +1,4 @@
+import datetime
 import io
 import json
 import logging
@@ -23,8 +24,23 @@ def load_json_data(fpath: Union[str, pathlib.Path]) -> Dict[str, str]:
 def save_json_data(data: List[Dict], fpath: Union[str, pathlib.Path]):
     fpath = _to_path(fpath).resolve()
     with fpath.open(mode="wt") as f:
-        json.dump(data, f, indent=2, ensure_ascii=False)
+        json.dump(data, f, indent=2, ensure_ascii=False, cls=ExtendedJSONEncoder)
     LOGGER.info("saved json data to %s", fpath)
+
+
+def load_text_data(fpath: Union[str, pathlib.Path]) -> str:
+    fpath = _to_path(fpath).resolve()
+    with fpath.open(mode="rt") as f:
+        data = f.read()
+    LOGGER.info("loaded text data from %s", fpath)
+    return data
+
+
+def save_text_data(data: str, fpath: Union[str, pathlib.Path]):
+    fpath = _to_path(fpath).resolve()
+    with fpath.open(mode="wt") as f:
+        f.write(data)
+    LOGGER.info("saved text data to %s", fpath)
 
 
 def _to_path(path: Union[str, pathlib.Path]) -> pathlib.Path:
@@ -37,3 +53,18 @@ def _to_path(path: Union[str, pathlib.Path]) -> pathlib.Path:
         raise TypeError(
             "`path` type invalid; must be {}, not {}".format({str, pathlib.Path}, type(path))
         )
+
+
+class ExtendedJSONEncoder(json.JSONEncoder):
+    """
+    Sub-class of :class:`json.JSONEncoder`, used to write JSON data to disk in
+    :func:`write_json()` while handling a broader range of Python objects.
+    - :class:`datetime.datetime` => ISO-formatted string
+    - :class:`datetime.date` => ISO-formatted string
+    """
+
+    def default(self, obj):
+        if isinstance(obj, (datetime.datetime, datetime.date)):
+            return obj.isoformat()
+        else:
+            return super().default(obj)
