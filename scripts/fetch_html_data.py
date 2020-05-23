cd91dcf63380e0cb802b51ff769979d4f8a799ca
Burton DeWilde <burtdewilde@gmail.com>
2020-05-23 14:29:43 -0500

    Refactor and rename parts of html data fetcher
    also prevent accidental overwrites as in rss data fetcher

--- scripts/fetch_html_data.py
+++ scripts/fetch_html_data.py
@@ -4,7 +4,7 @@ import pathlib
 import random
 import shutil
 import sys
-from typing import Any, Dict, Optional, Tuple
+from typing import Any, Dict, Optional, Tuple, Union
 
 import httpx
 
@@ -12,6 +12,7 @@ import dragnet_data
 
 logging.basicConfig(level=logging.INFO)
 
+PKG_ROOT = dragnet_data.utils.get_pkg_root()
 USER_AGENTS = (
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15",
@@ -19,85 +20,89 @@ USER_AGENTS = (
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:75.0) Gecko/20100101 Firefox/75.0",
 )
-_TOML_FIELDS = ("id", "url", "title", "dt_published", "text")
+_META_FIELDS = ("id", "url", "title", "dt_published", "text")
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Script to fetch HTML documents and some metadata for a set of pages "
+        description="Fetch HTML documents and some metadata for a set of pages "
         "published to a curated collection of RSS feeds -- and, when possible, "
-        "to extract a first pass on the page's main body text.",
+        "extract a first pass on the page's main body text.",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     add_arguments(parser)
     args = parser.parse_args()
+    args.pages_fpath = args.pages_fpath.resolve()
     args.data_dirpath = args.data_dirpath.resolve()
-    # fetch base data from rss feeds
-    rss_pages = []
-    feeds = dragnet_data.utils.load_toml_data(args.feeds_fpath)["feeds"]
-    for feed in feeds:
-        entries = dragnet_data.rss.get_entries_from_feed(feed, maxn=args.maxn_entries_per_feed)
-        rss_pages.extend(
-            dragnet_data.rss.get_data_from_entry(entry) for entry in entries
-        )
-    # just in case: remove any rss pages without urls, since we need them next
-    rss_pages = [rss_page for rss_page in rss_pages if rss_page.get("url")]
-    logging.info("got data for %s pages from RSS feeds", len(rss_pages))
-    dragnet_data.utils.save_toml_data(
-        {"pages": rss_pages}, args.data_dirpath.joinpath("rss_pages.toml"),
-    )
     # make html and meta directories if they don't already exist
-    args.data_dirpath.joinpath("html").mkdir(parents=False, exist_ok=True)
-    args.data_dirpath.joinpath("meta").mkdir(parents=False, exist_ok=True)
-    # fetch html and re-extract base metadata, plus text if available
+    for dirname in ("html", "meta"):
+        args.data_dirpath.joinpath(dirname).mkdir(parents=False, exist_ok=True)
+    rss_pages = dragnet_data.utils.load_toml_data(args.pages_fpath)["pages"]
+    # random shuffle page order to avoid hitting sites' servers too hard
     rss_pages = random.sample(rss_pages, k=len(rss_pages))
+    # fetch html and re-extract base metadata, plus text if available
     n_pages = len(rss_pages)
     with httpx.Client(timeout=args.http_timeout) as client:
-        for idx, rss_page in enumerate(rss_pages):
+        for idx, rss_page in enumerate(rss_pages[:3]):
             logging.info("getting data for page %s / %s", idx, n_pages)
             headers = {"user-agent": random.choice(USER_AGENTS)}
-            result = get_page_html_and_data(
+            data = get_page_html_and_meta_data(
                 rss_page["url"], client=client, headers=headers,
             )
-            if result is None:
+            if data is None:
                 continue
             else:
-                html, data = result
-                dragnet_data.utils.save_text_data(
-                    html, args.data_dirpath.joinpath("html", f"{data['id']}.html"),
-                )
-                dragnet_data.utils.save_toml_data(
-                    data, args.data_dirpath.joinpath("meta", f"{data['id']}.toml"),
-                )
+                html, meta = data
+                html_fpath = args.data_dirpath.joinpath("html", f"{meta['id']}.html")
+                meta_fpath = args.data_dirpath.joinpath("meta", f"{meta['id']}.toml")
+                save_page_data_or_log(html, html_fpath, args.force)
+                save_page_data_or_log(meta, meta_fpath, args.force)
     # package file collections into archives
-    shutil.make_archive(
-        args.data_dirpath.joinpath("html"),
-        root_dir=args.data_dirpath.joinpath("html"),
-        format="gztar",
-    )
-    shutil.make_archive(
-        args.data_dirpath.joinpath("meta"),
-        root_dir=args.data_dirpath.joinpath("meta"),
-        format="gztar",
-    )
+    for dirname in ("html", "meta"):
+        dragnet_data.utils.make_gztar_archive_from_dir(args.data_dirpath.joinpath(dirname))
 
 
-def add_arguments(parser):
+def save_page_data_or_log(
+    data: Union[str, Dict[str, Any]], fpath: pathlib.Path, force: bool,
+):
+    if fpath.exists() and force is False:
+        logging.warning(
+            "%s already exists and `force` is False; data will not be saved", fpath,
+        )
+    else:
+        if isinstance(data, str):
+            dragnet_data.utils.save_text_data(data, fpath)
+        elif isinstance(data, dict):
+            dragnet_data.utils.save_toml_data(data, fpath)
+        else:
+            raise TypeError(f"data type {type(data)} is invalid")
+
+
+def add_arguments(parser: argparse.ArgumentParser):
     parser.add_argument(
-        "--feeds_fpath", type=pathlib.Path, default=pathlib.Path("./data/rss_feeds.toml"),
+        "--pages_fpath",
+        type=pathlib.Path,
+        default=PKG_ROOT.parents[1].joinpath("data", "rss_pages.toml"),
+        help="path to file on disk where pages fetched from RSS feeds are stored",
     )
     parser.add_argument(
-        "--data_dirpath", type=pathlib.Path, default=pathlib.Path("./data"),
-    )
-    parser.add_argument(
-        "--maxn_entries_per_feed", type=int, default=25,
+        "--data_dirpath", type=pathlib.Path, default=PKG_ROOT.parents[1].joinpath("data"),
+        help="path to directory on disk under which HTML and meta data are to be stored "
+        "at `data_dirpath/html` and `data_dirpath/meta`, respectively",
     )
     parser.add_argument(
         "--http_timeout", type=float, default=5.0,
+        help="number of seconds to wait on all network operations before raising a timeout error",
+    )
+    parser.add_argument(
+        "--force", action="store_true", default=False,
+        help="if specified, save HTML and meta data under `data_dirpath` even if files "
+        "already exist in those locations; otherwise, just log a data preview to the console. "
+        "note: corresponding gztar archives are *always* saved to disk.",
     )
 
 
-def get_page_html_and_data(
+def get_page_html_and_meta_data(
     url: str,
     client: Optional[httpx.Client] = None,
     **kwargs,
@@ -108,16 +113,16 @@ def get_page_html_and_data(
         logging.exception("unable to get HTML for %s", url)
         return
     try:
-        data = dragnet_data.html.get_data_from_html(html)
+        meta = dragnet_data.html.get_data_from_html(html)
     except Exception:
         logging.error("unable to extract data from HTML for %s", url)
         return
-    if "url" not in data:
-        data["url"] = str(response.url)
-    data["id"] = dragnet_data.utils.generate_page_uuid(data["url"])
+    if "url" not in meta:
+        meta["url"] = str(response.url)
+    meta["id"] = dragnet_data.utils.generate_page_uuid(meta["url"])
     # for convenience, let's standardize the order of fields in output data
-    data = {field: data.get(field) for field in _TOML_FIELDS}
-    return (html, data)
+    meta = {field: meta.get(field) for field in _META_FIELDS}
+    return (html, meta)
 
 
 if __name__ == "__main__":
