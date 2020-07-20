0bede298e56183bb0a5bc819bd12c2c41b716a49
Burton DeWilde <burtdewilde@gmail.com>
2020-07-19 21:18:46 -0500

    Improve docs and add utils funcs for convenience

--- src/dragnet_data/utils.py
+++ src/dragnet_data/utils.py
@@ -11,14 +11,20 @@ from typing import Any, Dict, List, Optional, Union
 import toml
 
 LOGGER = logging.getLogger(__name__)
+DATA_DIRNAMES = ("html", "meta")
 
 
-def get_random_sample(items: List[Any], n: int) -> List[Any]:
+def get_random_sample(items: List[Any], k: int) -> List[Any]:
     """
-    Thin wrapper around ``random.sample`` that takes the min of ``n`` and ``len(items)``
+    Thin wrapper around ``random.sample`` that takes the min of ``k`` and ``len(items)``
     when sampling, so as to avoid a ``ValueError``.
     """
-    return random.sample(items, min(n, len(items)))
+    return random.sample(items, min(k, len(items)))
+
+
+def generate_page_uuid(url: str) -> str:
+    """Generate a consistent UUID based on the MD5 hash of a URL."""
+    return str(uuid.uuid3(uuid.NAMESPACE_URL, url))
 
 
 def get_pkg_root() -> Optional[pathlib.Path]:
@@ -29,11 +35,7 @@ def get_pkg_root() -> Optional[pathlib.Path]:
         return None
 
 
-def generate_page_uuid(url: str) -> str:
-    return str(uuid.uuid3(uuid.NAMESPACE_URL, url))
-
-
-def load_rss_feeds() -> List[Dict[str, str]]:
+def load_rss_feeds() -> List[Dict[str, Any]]:
     """
     Load a curated collection of RSS feeds from which recent pages may be fetched,
     as stored in ``/path/to/dragnet_data/data/rss_feeds.toml``.
@@ -42,6 +44,41 @@ def load_rss_feeds() -> List[Dict[str, str]]:
     feeds_fpath = pkg_root.parents[1].joinpath("data", "rss_feeds.toml")
     feeds = load_toml_data(feeds_fpath)["feeds"]
     return feeds
+
+
+def load_rss_pages(fpath: Union[str, pathlib.Path]) -> List[Dict[str, Any]]:
+    """
+    Convenience function for loading standard RSS pages data from disk.
+
+    See Also:
+        :func:`save_rss_pages()`
+    """
+    return load_toml_data(fpath)["pages"]
+
+
+def save_rss_pages(data: List[Dict[str, Any]], fpath: Union[str, pathlib.Path]):
+    """
+    Convenience function for saving standard RSS pages data to disk.
+
+    See Also:
+        :func:`load_rss_pages()`
+    """
+    save_toml_data({"pages": data}, fpath)
+
+
+def save_toml_data(data: Dict[str, Any], fpath: Union[str, pathlib.Path]):
+    fpath = to_path(fpath).resolve()
+    with fpath.open(mode="wt") as f:
+        toml.dump(data, f)
+    LOGGER.info("saved toml data to %s", fpath)
+
+
+def load_toml_data(fpath: Union[str, pathlib.Path]) -> Dict[str, Any]:
+    fpath = to_path(fpath).resolve()
+    with fpath.open(mode="rt") as f:
+        data = toml.load(f)
+    LOGGER.info("loaded toml data from %s", fpath)
+    return dict(data)
 
 
 def load_json_data(fpath: Union[str, pathlib.Path]) -> Dict[str, str]:
@@ -59,11 +96,31 @@ def save_json_data(data: List[Dict], fpath: Union[str, pathlib.Path]):
     LOGGER.info("saved json data to %s", fpath)
 
 
+def save_text_data(data: Union[str, List[str]], fpath: Union[str, pathlib.Path]):
+    """
+    Save text data to disk at ``fpath``,
+    either all-together (data: str) or line-by-line (data: List[str]).
+    """
+    fpath = to_path(fpath).resolve()
+    with fpath.open(mode="wt") as f:
+        if isinstance(data, str):
+            f.write(data)
+        elif isinstance(data, list):
+            f.writelines(f"{line}\n" for line in data)
+        else:
+            raise TypeError(f"data must be {(str, list)}, but got {type(data)}")
+    LOGGER.info("saved text data to %s", fpath)
+
+
 def load_text_data(
     fpath: Union[str, pathlib.Path],
     *,
     lines: bool = False,
 ) -> Union[str, List[str]]:
+    """
+    Load text data from disk at ``fpath``,
+    either all-together (-> str) or line-by-line (-> List[str]).
+    """
     fpath = to_path(fpath).resolve()
     with fpath.open(mode="rt") as f:
         if not lines:
@@ -72,28 +129,6 @@ def load_text_data(
             data = [line.strip() for line in f]
     LOGGER.info("loaded text data from %s", fpath)
     return data
-
-
-def save_text_data(data: str, fpath: Union[str, pathlib.Path]):
-    fpath = to_path(fpath).resolve()
-    with fpath.open(mode="wt") as f:
-        f.write(data)
-    LOGGER.info("saved text data to %s", fpath)
-
-
-def load_toml_data(fpath: Union[str, pathlib.Path]) -> Dict[str, Any]:
-    fpath = to_path(fpath).resolve()
-    with fpath.open(mode="rt") as f:
-        data = toml.load(f)
-    LOGGER.info("loaded toml data from %s", fpath)
-    return data
-
-
-def save_toml_data(data: Dict[str, Any], fpath: Union[str, pathlib.Path]):
-    fpath = to_path(fpath).resolve()
-    with fpath.open(mode="wt") as f:
-        toml.dump(data, f)
-    LOGGER.info("saved toml data to %s", fpath)
 
 
 def to_path(path: Union[str, pathlib.Path]) -> pathlib.Path:
